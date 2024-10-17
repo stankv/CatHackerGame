0906ef8fd21e35790eb4cb98f4fcd38c2505746e
stankv <stankv@mail.ru>
2024-10-17 13:44:50 +0300

    simple command handlers and input text added

--- src/main/java/com/javarush/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/com/javarush/telegrambot/MyFirstTelegramBot.java
@@ -9,12 +9,14 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import com.javarush.telegrambot.SettingsBot;
 
+import java.util.Map;
+
 import static com.javarush.telegrambot.SettingsBot.BOT_NAME;
 import static com.javarush.telegrambot.SettingsBot.BOT_TOKEN;
 
 public class MyFirstTelegramBot extends MultiSessionTelegramBot {
-    public static final String NAME = BOT_NAME; // TODO: добавьте имя бота в кавычках
-    public static final String TOKEN = BOT_TOKEN; //TODO: добавьте токен бота в кавычках
+    public static final String NAME = BOT_NAME;
+    public static final String TOKEN = BOT_TOKEN;
 
     public MyFirstTelegramBot() {
         super(NAME, TOKEN);
@@ -22,8 +24,40 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
     @Override
     public void onUpdateEventReceived(Update updateEvent) {
-        // TODO: основной функционал бота будем писать здесь
-        sendTextMessageAsync("Привет!");
+        // обработчики команд бота и введенного текста
+        if (getMessageText().equals("/start")) {
+            sendTextMessageAsync("Привет!");
+        }
+
+        if (getMessageText().equals("/bye")) {
+            sendTextMessageAsync("Пока!");
+        }
+
+        if (getMessageText().contains("бомба")) {
+            sendTextMessageAsync("Опасность!");
+        }
+
+        if (getMessageText().contains("картинка")) {
+            sendPhotoMessageAsync("step_8_pic");
+        }
+
+        if (getMessageText().contains("кот")) {
+            sendTextMessageAsync("Выбери номер кота: ",
+                    Map.of("кот 1", "cat1", "кот 2", "cat2"));
+        }
+
+
+        // Обработчики нажатий кнопок
+
+        if (getCallbackQueryButtonKey().equals("cat1")) {
+            sendPhotoMessageAsync("step_1_pic");
+        }
+
+        if (getCallbackQueryButtonKey().equals("cat2")) {
+            sendPhotoMessageAsync("step_2_pic");
+        }
+
+
     }
 
     public static void main(String[] args) throws TelegramApiException {
